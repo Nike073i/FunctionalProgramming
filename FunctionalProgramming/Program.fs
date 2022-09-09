041eb571fa4c0cdf86dd49351dddc3132d337192
= <nike073i@mail.ru>
2022-09-09 19:28:22 +0400

    Добавьте файлы проекта.

---
+++ FunctionalProgramming/Program.fs
@@ -0,0 +1,9 @@
+﻿open System
+[<EntryPoint>]
+let main (args: string[]) = 
+    if args.Length <> 2 then
+        failwith "Error: expected 2 arguments"
+    let greeting, thing = args[0], args[1]
+    let timeOfDay = DateTime.Now.ToString("hh:mm tt")
+    printf "%s, %s at %s" greeting thing timeOfDay
+    0
